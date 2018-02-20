c3d4e7963d752a2a3726edf842cf9253e8bd4c2a
Nikke <nikke_marjomaa@hotmail.com>
2018-02-20 12:03:09 +0200

    coment

--- Peliohjelmointi 1/Assets/Scripts/PlayerController.cs
+++ Peliohjelmointi 1/Assets/Scripts/PlayerController.cs
@@ -4,31 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float Speed;
+    private CharacterController controller;
 
-    public float speed;             //Floating point variable to store the player's movement speed.
-
-    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
-
-    // Use this for initialization
     void Start()
     {
-        //Get and store a reference to the Rigidbody2D component so that we can access it.
-        rb2d = GetComponent<Rigidbody2D>();
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis("Horizontal");
-
-        //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis("Vertical");
-
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.AddForce(movement * speed);
+        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * Speed);
     }
+
 }
