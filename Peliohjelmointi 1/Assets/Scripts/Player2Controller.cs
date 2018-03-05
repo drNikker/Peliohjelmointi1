8217ea46fbccb1b5617f0077fa28b3caf946a94a
Nikke <nikke_marjomaa@hotmail.com>
2018-03-05 22:12:14 +0200

    A lot of stuff

---
+++ Peliohjelmointi 1/Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player2Controller : MonoBehaviour
+{
+    public float Speed;
+    Animator animator;
+    private Rigidbody2D player2;
+    public bool punching;
+    public Slider healthSlider;
+    public int EnemyHealth;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        player2 = GetComponent<Rigidbody2D>();
+        EnemyHealth = 100;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal2");
+        float moveVertical = Input.GetAxis("Vertical2");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        player2.AddForce(movement * Speed);
+
+        if (Input.GetKeyDown("u"))
+        {
+            
+            animator.SetTrigger("leftpunch");
+            
+        }
+
+        if (Input.GetKeyDown("o"))
+        {
+            
+            animator.SetTrigger("rightpunch");
+            
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("hand1") && punching)
+        { 
+            Debug.Log("Hit");
+            EnemyHealth -= 10;
+            healthSlider.value = EnemyHealth;
+            
+        }
+
+    }
+
+    public void PunchStart()
+    {
+        punching = true;
+    }
+
+    public void PunchEnd()
+    {
+        punching = false;
+    }
+}
--- Peliohjelmointi 1/Assets/Scripts/PlayerController.cs
+++ Peliohjelmointi 1/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float Speed;
-    private CharacterController controller;
+    Animator animator;
+    private Rigidbody2D player1;
+    public bool punching;
+    public Slider healthSlider;
+    public int EnemyHealth;
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        player1 = GetComponent<Rigidbody2D>();
+        EnemyHealth = 100;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * Speed);
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        player1.AddForce(movement * Speed);
+
+        if (Input.GetKeyDown("q"))
+        {
+           
+            animator.SetTrigger("leftpunch");
+            
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+           
+            animator.SetTrigger("rightpunch");
+            
+        }
+
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("hand2") && punching)
+        {
+            Debug.Log("Hit");
+            EnemyHealth -= 10;
+            healthSlider.value = EnemyHealth;
+        }
+
     }
 
+    public void PunchStart()
+    {
+        punching = true;
+    }
+
+    public void PunchEnd()
+    {
+        punching = false;
+    }
 }
+
